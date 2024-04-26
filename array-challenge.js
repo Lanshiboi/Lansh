96956a072db642441bf4fd07bc3ce73badcf7395
Lanshiboi <168156338+Lanshiboi@users.noreply.github.com>
2024-04-26 12:54:06 +0800

    Add files via upload

---
+++ array-challenge.js
@@ -0,0 +1,31 @@
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+/*
+    Challenge: Display the numbers using the below format
+    EVEN NUMBERS: 
+    2
+    4
+    6
+    8
+    10
+    ODD NUMBERS: 
+    1
+    3
+    5
+    7
+    9
+*/
+
+console.log("EVEN NUMBERS:");
+arr.forEach(num => {
+    if (num % 2 === 0) {
+        console.log(num);
+    }
+});
+
+console.log("ODD NUMBERS:");
+arr.forEach(num => {
+    if (num % 2 !== 0) {
+        console.log(num);
+    }
+});
---
+++ number-challenge.js
@@ -0,0 +1,24 @@
+/*
+    1 Create a function that will return the sum 3 numbers
+    2 Create a function that will return the difference of the 2 numbers
+    3 Create a function that will return the product of the 2 numbers
+    4 Create a function that will return the average of the 2 numbers
+*/
+
+function sumOfThreeNumbers(a, b, c) {
+    return a + b + c;
+}
+
+function differenceOfTwoNumbers(a, b) {
+    return a - b;
+}
+
+
+function productOfTwoNumbers(a, b) {
+    return a * b;
+}
+
+
+function averageOfTwoNumbers(a, b) {
+    return (a + b) / 2;
+}
